ecf2be855fe1d633b5db446d64021cce0a80a342
Mi <djdeffkid@gmail.com>
2022-06-14 20:35:58 -0400

    Update mob_skills.sql

--- sql/mob_skills.sql
+++ sql/mob_skills.sql
@@ -3537,7 +3537,7 @@ INSERT INTO `mob_skills` VALUES (3706,633,'cross_reaver',4,7.0,2000,1500,4,0,0,0
 -- INSERT INTO `mob_skills` VALUES (3707,3451,'#1915',0,7.0,2000,1500,4,0,0,0,0,0,0);
 INSERT INTO `mob_skills` VALUES (3708,641,'swift_blade',0,7.0,2000,1500,4,0,0,0,9,0,0);          -- AAHM
 INSERT INTO `mob_skills` VALUES (3709,2283,'chant_du_cygne',0,7.0,2000,1500,4,0,0,0,13,10,0);    -- AAHM
-INSERT INTO `mob_skills` VALUES (3710,2285,'arrogance_incarnate',0,7.0,2000,1500,4,0,0,0,0,0,0); -- AAEV
+INSERT INTO `mob_skills` VALUES (3710,2285,'arrogance_incarnate',1,7.0,2000,1500,4,0,0,0,0,0,0); -- AAEV
 INSERT INTO `mob_skills` VALUES (3711,643,'vorpal_blade',0,7.0,2000,1500,4,0,0,0,4,8,0);         -- AAEV
 INSERT INTO `mob_skills` VALUES (3712,635,'dominion_slash',1,7.0,2000,1500,4,0,0,0,0,0,0);       -- AAEV
 INSERT INTO `mob_skills` VALUES (3713,2284,'chant_du_cygne',0,7.0,2000,1500,4,0,0,0,13,10,0);    -- AAEV
