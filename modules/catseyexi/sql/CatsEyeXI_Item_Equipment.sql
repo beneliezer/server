cc1f3df719b615f7f5f92e7c85c1a260fd82902b
Grave Taker <bsm267@yahoo.com>
2022-07-03 18:52:15 -0400

    added the item mods
    added the item mods that strong did into modules

---
+++ modules/catseyexi/sql/CatsEyeXI_Item_Equipment.sql
@@ -0,0 +1,5 @@
+-- Diamond buckler
+INSERT INTO `item_equipment` VALUES (26410, 'diamond_buckler', 1, 0, 4194303, 659, 1, 0, 2, 0, 0); -- Diamond buckler shield
+
+-- Kupo Suit
+UPDATE `item_equipment` SET `rslot`='128' WHERE `itemId`=25726; -- Block Leg equipment
---
+++ modules/catseyexi/sql/CatsEyeXI_Item_mods.sql
@@ -0,0 +1,7 @@
+-- Kupo Suit
+INSERT INTO `item_mods` VALUES (25726, 169, 18); -- 18% movement speed
+INSERT INTO `item_mods` VALUES (25726, 1, 1); -- 1 DEF
+
+
+-- Diamond buckler
+INSERT INTO `item_mods` VALUES (26410, 1, 1); -- 1 DEF
