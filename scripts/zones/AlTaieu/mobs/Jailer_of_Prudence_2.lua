b2a30938dfbc13d5e6b6f98123548a64a4cc4828
HaxPrime <108164867+HaxPrime@users.noreply.github.com>
2022-11-03 15:35:45 -0500

    Create Jailer_of_Prudence_2.lua
    This PR is part of a three part fix to give functionality to our Jailer of Prudence module. We need to distinguish between the two Jailer of Prudence monsters for over rides to correctly apply. This PR creates a dummy file for over rides to be applied to.

---
+++ scripts/zones/AlTaieu/mobs/Jailer_of_Prudence_2.lua
@@ -0,0 +1,30 @@
+-----------------------------------
+-- Area: Al'Taieu
+-- NM: Jailer of Prudence_2
+-- Dummy file required for module functionality
+-- AnimationSubs: 0 - Normal, 3 - Mouth Open
+-----------------------------------
+local ID = require("scripts/zones/AlTaieu/IDs")
+mixins = { require("scripts/mixins/job_special") }
+mixins = {require("scripts/mixins/rage")}
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+-----------------------------------
+local entity = {}
+
+entity.onMobInitialize = function(mob)
+end
+
+entity.onMobSpawn = function(mob)
+end
+
+entity.onMobFight = function(mob, target)
+end
+
+entity.onMobDespawn = function(mob)
+end
+
+entity.onMobDeath = function(mob, player, optParams)
+end
+
+return entity
