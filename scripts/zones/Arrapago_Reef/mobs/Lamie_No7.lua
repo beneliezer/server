2b90d951f7df71d4db031fd6b919ef25b0f5ce2e
TonzoffunCE <tonzoffunce@gmail.com>
2023-01-18 01:51:33 -0500

    Added NM luas
    Added the NM luas that needed to be added for Daily Quests

---
+++ scripts/zones/Arrapago_Reef/mobs/Lamie_No7.lua
@@ -0,0 +1,15 @@
+-----------------------------------
+-- Area: Arrapago Reef
+--  Mob: Lamie No7
+-- Note: 
+-----------------------------------
+local ID = require("scripts/zones/Arrapago_Reef/IDs")
+require("scripts/globals/mobs")
+-----------------------------------
+local entity = {}
+
+entity.onMobDeath = function(mob, player, optParams)
+end
+
+return entity
+
