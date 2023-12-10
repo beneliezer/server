b196441ba05369c16b68f3f0cda14c6e7a0f1434
Tal Ben-Eliezer <beneliezer@gmail.com>
2023-12-10 12:44:21 -0500

    fix boss door

--- scripts/zones/Zhayolm_Remnants/npcs/_21h.lua
+++ scripts/zones/Zhayolm_Remnants/npcs/_21h.lua
@@ -9,7 +9,7 @@ end
 entity.onEventFinish = function(player, csid, option, npc)
     if csid == 300 and option == 1 then
         local instance = npc:getInstance()
-        instance:getEntity(bit.band(npc:getID() + 1, 0xFFF), xi.objType.NPC):setStatus(xi.status.DISAPPEAR)
+        -- instance:getEntity(bit.band(npc:getID() + 1, 0xFFF), xi.objType.NPC):setStatus(xi.status.DISAPPEAR)
         xi.salvageUtil.onDoorOpen(npc)
     end
 end
--- scripts/zones/Zhayolm_Remnants/npcs/_21i.lua
+++ scripts/zones/Zhayolm_Remnants/npcs/_21i.lua
@@ -15,8 +15,8 @@ entity.onEventFinish = function(player, csid, option, npc)
     if csid == 300 and option == 1 then
         local instance = npc:getInstance()
 
-        instance:getEntity(bit.band(npc:getID() - 1, 0xFFF), xi.objType.NPC):setStatus(xi.status.DISAPPEAR)
-        instance:getEntity(bit.band(npc:getID(), 0xFFF), xi.objType.NPC):setStatus(xi.status.DISAPPEAR)
+        -- instance:getEntity(bit.band(npc:getID() - 1, 0xFFF), xi.objType.NPC):setStatus(xi.status.DISAPPEAR)
+        -- instance:getEntity(bit.band(npc:getID(), 0xFFF), xi.objType.NPC):setStatus(xi.status.DISAPPEAR)
         instance:getEntity(bit.band(npc:getID() - 1, 0xFFF), xi.objType.NPC):setAnimation(8)
         instance:getEntity(bit.band(npc:getID(), 0xFFF), xi.objType.NPC):setAnimation(8)
     end
