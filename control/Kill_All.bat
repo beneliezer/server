3960424b9fe07925ee8dad091d48a339df01f635
Tal Ben-Eliezer <beneliezer@gmail.com>
2024-01-09 15:15:45 -0500

    update startup scripts

--- control/Kill_All.bat
+++
@@ -1,6 +0,0 @@
-taskkill /f /im xi_map.exe
-taskkill /f /im xi_connect.exe
-taskkill /f /im xi_search.exe
-taskkill /f /im xi_world.exe
-timeout /t 5 /nobreak
-taskkill /f /im cmd.exe
--- control/Start_World.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title World Server
-cd ..
-:onCrash
-echo [%date% %time%] Restarting World Server...
-xi_world.exe
-echo ...
-GOTO onCrash
