19100266c4fc543f815292f6ee7a0aec59d09b55
MowFord <131182600+MowFord@users.noreply.github.com>
2024-04-07 11:26:06 -0500

    Better messaging

--- tools/apply_patches.py
+++ tools/apply_patches.py
@@ -22,11 +22,11 @@ def apply_patches():
                 # Use subprocess to run the 'git apply' command
                 # subprocess.run(["git", "apply", "-R", patch_path], cwd="../")
                 subprocess.run(["git", "apply", "--ignore-whitespace", "-v", patch_path], cwd="../", check=True)
-                print(f"Applied patch: {filename}")
+                print(f"SUCCESS: Applied patch: {filename}")
             except subprocess.CalledProcessError as e:
-                print(f"Error applying patch {filename}: {e}")
+                print(f"ERROR: applying patch {filename}: {e}")
             except Exception as e:
-                print(f"Unknown error applying patch {filename}: {e}")
+                print(f"ERROR: Unknown applying patch {filename}: {e}")
 
 if __name__ == "__main__":
     apply_patches()
