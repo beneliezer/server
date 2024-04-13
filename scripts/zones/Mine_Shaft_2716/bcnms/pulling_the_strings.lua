7d49613f828267f250ace9709ccc7939df9f40df
Tal Ben-Eliezer <beneliezer@gmail.com>
2024-04-13 18:05:33 -0400

    enable pulling the strings as well

--- scripts/zones/Mine_Shaft_2716/bcnms/pulling_the_strings.lua
+++ scripts/zones/Mine_Shaft_2716/bcnms/pulling_the_strings.lua
@@ -1,7 +1,7 @@
 -----------------------------------
--- Bionic Bug ENM
+-- ENM: Pulling the Strings
 -- Zone: Mine Shaft #2716
--- KI
+-- Keyitem: SHAFT_GATE_OPERATING_DIAL
 -----------------------------------
 require('scripts/globals/battlefield')
 -----------------------------------
@@ -19,15 +19,15 @@ battlefieldObject.onBattlefieldRegister = function(player, battlefield)
 end
 
 battlefieldObject.onBattlefieldEnter = function(player, battlefield)
-    if player:hasKeyItem(xi.ki.SHAFT_2716_OPERATING_LEVER) then
-        player:delKeyItem(xi.ki.SHAFT_2716_OPERATING_LEVER)
+    if player:hasKeyItem(xi.ki.SHAFT_GATE_OPERATING_DIAL) then
+        player:delKeyItem(xi.ki.SHAFT_GATE_OPERATING_DIAL)
     end
 end
 
 battlefieldObject.onBattlefieldLeave = function(player, battlefield, leavecode)
     if leavecode == xi.battlefield.leaveCode.WON then
         local _, clearTime, partySize = battlefield:getRecord()
-        player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar('[cs]bit'), 0)
+        player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), 0)
     elseif leavecode == xi.battlefield.leaveCode.LOST then
         player:startEvent(32002)
     end
@@ -38,7 +38,7 @@ end
 
 battlefieldObject.onEventFinish = function(player, csid, option)
     if csid == 32001 then
-        player:addExp(3500)
+        player:addExp(4000)
     end
 end
 
