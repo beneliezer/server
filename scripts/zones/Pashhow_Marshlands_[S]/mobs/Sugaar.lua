461e566ee67bb89561a0483fafbb360de0384969
Spicyryan <30642255+Spicyryan@users.noreply.github.com>
2024-04-21 16:22:32 -0400

    Sugaar

--- scripts/zones/Pashhow_Marshlands_[S]/mobs/Sugaar.lua
+++ scripts/zones/Pashhow_Marshlands_[S]/mobs/Sugaar.lua
@@ -5,11 +5,12 @@
 local entity = {}
 
 entity.onMobInitialize = function(mob)
+    mob:setMod(xi.mod.PETRIFYRES, 10000)
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
 end
 
 entity.onAdditionalEffect = function(mob, target, damage)
-    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.SILENCE)
+    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.SILENCE, { chance = 15, duration = math.random(15, 30) })
 end
 
 entity.onMobDeath = function(mob, player, optParams)
