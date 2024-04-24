400ec02237edd96fa1c25e21c5f4188f6f92b74d
MowFord <131182600+MowFord@users.noreply.github.com>
2024-04-24 18:44:34 -0500

    [lua] Nommo NM
    Co-authored-by: Spicyryan <30642255+Spicyryan@users.noreply.github.com>

--- scripts/zones/Pashhow_Marshlands_[S]/mobs/Nommo.lua
+++ scripts/zones/Pashhow_Marshlands_[S]/mobs/Nommo.lua
@@ -1,8 +1,19 @@
 -----------------------------------
 -- Area: Pashhow Marshlands [S]
 --   NM: Nommo
+-- https://www.bg-wiki.com/ffxi/Nommo
 -----------------------------------
 local entity = {}
+
+entity.onMobInitialize = function(mob)
+    mob:setMod(xi.mod.PETRIFYRES, 10000)
+    mob:addMod(xi.mod.MOVE_SPEED_STACKABLE, 25)
+    mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
+end
+
+entity.onAdditionalEffect = function(mob, target, damage)
+    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.ENAMNESIA, { chance = 10, duration = 30 })
+end
 
 entity.onMobDeath = function(mob, player, optParams)
     xi.hunts.checkHunt(mob, player, 509)
