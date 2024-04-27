16d5bdd8116ea51bf7abd0fee726efefcf636d01
Spicyryan <30642255+Spicyryan@users.noreply.github.com>
2024-04-27 18:18:51 -0400

    Update petrify immunity with new addition to immunities file

--- scripts/zones/Pashhow_Marshlands_[S]/mobs/Sugaar.lua
+++ scripts/zones/Pashhow_Marshlands_[S]/mobs/Sugaar.lua
@@ -5,12 +5,11 @@
 local entity = {}
 
 entity.onMobInitialize = function(mob)
-    mob:setMod(xi.mod.PETRIFYRES, 10000)
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
 end
 
 entity.onAdditionalEffect = function(mob, target, damage)
-    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.SILENCE, { chance = 15, duration = math.random(15, 30) })
+    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.SILENCE, { chance = 15, duration = 30 })
 end
 
 entity.onMobDeath = function(mob, player, optParams)
