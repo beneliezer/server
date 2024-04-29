1dea09e206e67073439fee3437f82ae5babcf5a2
Spicyryan <30642255+Spicyryan@users.noreply.github.com>
2024-04-28 23:49:25 -0400

    Sarcopsylla

--- scripts/zones/Grauberg_[S]/mobs/Sarcopsylla.lua
+++ scripts/zones/Grauberg_[S]/mobs/Sarcopsylla.lua
@@ -7,11 +7,12 @@ mixins = { require('scripts/mixins/families/chigoe') }
 local entity = {}
 
 entity.onMobInitialize = function(mob)
+    mob:setMod(xi.mod.TRIPLE_ATTACK, 100)
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
 end
 
 entity.onAdditionalEffect = function(mob, target, damage)
-    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.ENFIRE)
+    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.ENFIRE, { power = math.random(25, 30) })
 end
 
 entity.onMobDeath = function(mob, player, optParams)
