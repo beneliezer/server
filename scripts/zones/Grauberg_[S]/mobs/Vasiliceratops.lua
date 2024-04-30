27af7921c2e62d5f53bec10852dd85ac8b6fa81c
Spicyryan <30642255+Spicyryan@users.noreply.github.com>
2024-04-29 23:41:05 -0400

    begin working on Vasiliceratops

--- scripts/zones/Grauberg_[S]/mobs/Vasiliceratops.lua
+++ scripts/zones/Grauberg_[S]/mobs/Vasiliceratops.lua
@@ -1,8 +1,13 @@
 -----------------------------------
 -- Area: Grauberg [S]
 --   NM: Vasiliceratops
+-- https://www.bg-wiki.com/ffxi/Vasiliceratops
 -----------------------------------
 local entity = {}
+
+entity.onMobInitialize = function(mob)
+    mob:setMod(xi.mod.DOUBLE_ATTACK, 100)
+end
 
 entity.onMobDeath = function(mob, player, optParams)
     xi.hunts.checkHunt(mob, player, 505)
